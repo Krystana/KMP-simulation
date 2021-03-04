957dd6775072fcb4af2042f8339dc9179c8228bb
M. Murat Ardag <mahmut.murat.ardag@uol.de>
2021-03-04 09:30:13 +0100

    Delete simulate_customers directory

--- simulate_customers/simulate_customers_Krystana.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-import numpy as np 
-
-transition_matrix = transition_matrix = pd.read_csv("./data/transition_matrix.csv")
-transition_matrix.set_index('location', inplace = True)
-
-print(transition_matrix)
